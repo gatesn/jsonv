b6353f427ba10cc1225a0353bb0abbe452f01162
Nicholas Gates <nickgatzgates@gmail.com>
2016-07-31 20:05:10 +0100

    Print test stuff

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
+#!/usr/bin/env python
+import json
 from jsonv import jsonv
 
-
-def main():
-    m = jsonv.loads("""
+SRC = """
     {
         "array": [hello, world],
         "key": value,
@@ -13,12 +13,37 @@ def main():
         "floaty": 10.5,
         "stringy": "foo"
     }
-    """)
-    print m.bind({
-        "world": 12345,
-        "hello": "abcd",
-        "value": [1, 2, 3]
-    })
+    """
+
+BINDINGS = {
+    "world": 12345,
+    "hello": "abcd",
+    "value": [1, 2, 3]
+}
+
+BOUND = """
+    {
+        "array": ["abcd", 12345],
+        "key": [1,2,3],
+        "nothing": null,
+        "truthy": true,
+        "falsey": false,
+        "number": 10,
+        "floaty": 10.5,
+        "stringy": "foo"
+    }
+    """
+
+
+def main():
+    print "JSONv:", SRC
+    m = jsonv.loads(SRC)
+
+    print "Bindings:", BINDINGS
+    bound = m.bind(BINDINGS)
+
+    print "Bound:", bound
+    assert bound == json.loads(BOUND)
 
 
 if __name__ == "__main__":
